be492dfecfce9c9dc649b0ff13394802887b64ff
George <georgie.george7@gmail.com>
2023-06-30 10:37:52 +0400

    add changes according to comments

--- home.js
+++ home.js
@@ -21,37 +21,37 @@ const dungeonMaster = [
   {
     image: './assets/Speakers/pexels-gioele-fazzeri-4496725.jpg',
     name: 'Gabriel Falconer',
-    hosted: 'Tournaments Hosted: 6',
+    hosted: 'Tournaments Hosted : 6',
     description: 'Revolves around epic tales of dwarven craftsmanship and legendary artifacts. In the Forge of Legends, players delve into ancient mines, master the art of smithing, and create legendary weapons and armor. Alongside crafting, players face formidable challenges and undertake quests to secure their place in the annals of dwarven lore.',
   },
   {
     image: './assets/Speakers/OliviaGreenfield.jpg',
     name: 'Olivia Greenfield',
-    hosted: 'Tournaments Hosted: 9',
+    hosted: 'Tournaments Hosted : 9',
     description: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
   },
   {
     image: './assets/Speakers/pexels-rene-asmussen-25758.jpg',
     name: 'Benjamin Smith',
-    hosted: 'Tournaments Hosted: 12',
+    hosted: 'Tournaments Hosted : 12',
     description: 'Specializes in high-octane, combat-focused adventures. In the Battlegrounds of Valor, players participate in grand-scale battles, epic duels, and daring quests that test their tactical skills and valor. With intense combat encounters and strategic decision-making, this game experience is perfect for players seeking thrilling action and heroic feats.',
   },
   {
     image: './assets/Speakers/samuel steelgrave.jpg',
     name: 'Samuel Steelgrave',
-    hosted: 'Tournaments Hosted: 4',
+    hosted: 'Tournaments Hosted : 4',
     description: 'Creates an immersive and atmospheric game experience where players find themselves in a dark and mysterious realm known as Oblivion. The campaign is filled with suspense, eerie encounters, and challenging puzzles, as players navigate through shadowy landscapes and unravel the secrets of this twisted dimension.',
   },
   {
     image: './assets/Speakers/stella Astrid.jpg',
     name: 'Stella Astrid',
-    hosted: 'Tournaments Hosted: 12',
+    hosted: 'Tournaments Hosted : 12',
     description: 'Revolves around epic tales of dwarven craftsmanship and legendary artifacts. In the Forge of Legends, players delve into ancient mines, master the art of smithing, and create legendary weapons and armor. Alongside crafting, players face formidable challenges and undertake quests to secure their place in the annals of dwarven lore.',
   },
   {
     image: './assets/Speakers/pexels-italo-melo-2379004.jpg',
     name: 'Roger Nightingale',
-    hosted: 'Tournaments Hosted: 9',
+    hosted: 'Tournaments Hosted : 9',
     description: 'Takes players on an epic journey of magical exploration and power. In Arcane Ascendance, players delve into ancient ruins, learn forgotten spells, and uncover artifacts of immense power. As the campaign progresses, players can ascend to new heights of arcane mastery, developing unique abilities and shaping the world around them.',
   },
 ];
@@ -66,7 +66,7 @@ for (let i = 0; i < dungeonMaster.length; i += 1) {
             <div class="speakerDetails">
               <h2>${dungeonMaster[i].name}</h2>
               <h5>${dungeonMaster[i].hosted}</h5>
-              <div class="ruler"></div>
+              <div class="ruler1"></div>
               <p>${dungeonMaster[i].description}</p>
             </div>
           </div>
